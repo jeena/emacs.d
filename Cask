735d0e046e372343855a344c094d826a65cb657c
jeena <spam@jeenaparadies.net>
2014-03-09 13:57:07 +0100

    initial commit

---
+++ Cask
@@ -0,0 +1,8 @@
+(source melpa)
+(source marmalade)
+
+(depends-on "flx-ido")
+(depends-on "ido-vertical-mode")
+(depends-on "smartparens")
+(depends-on "magit")
+
---
+++ init.el
@@ -0,0 +1,57 @@
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(show-paren-mode 1)
+(line-number-mode 1)
+(column-number-mode 1)
+(setq tab-width 4)
+(setq-default tab-width 4)
+(setq indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)
+(setq backup-inhibited 1)
+(setq-default backup-inhibited 1)
+(setq auto-save-default nil)
+(setq-default auto-save-default nil)
+(setq auto-save-list-file-prefix nil)
+(setq-default auto-save-list-file-prefix nil)
+
+(when (fboundp 'global-font-lock-mode) 
+  (global-font-lock-mode t)
+  (setq font-lock-maximum-decoration t))
+
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+
+(setq inhibit-splash-screen t)
+
+(defun kill-all-buffers ()
+  (interactive)
+  (mapcar 'kill-buffer (buffer-list))
+  (delete-other-windows))
+
+(global-set-key (kbd "C-x K") 'kill-all-buffers)
+
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flexz-matching t
+      redisplay-dont-pause t
+      ido-use-faces nil)
+
+(require 'flx-ido)
+(flx-ido-mode 1)
+
+(require 'ido-vertical-mode)
+(ido-vertical-mode t)
+
+(custom-set-variables
+ '(ido-vertical-define-keys 'C-n-C-p-up-and-down))
+
+(autoload 'magit-status "magit" nil t)
+(global-set-key (kbd "C-c C-a") 'magit-status)
+
+(require 'smartparens-config)
+(smartparens-global-mode)
+(show-smartparens-global-mode t)
+
