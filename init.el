54cffd52bcbfad034e491322da7d9c3e20a10400
Richard Pannek <richard.pannek@pelagicore.com>
2015-02-24 12:43:52 +0100

    added markdown mode

--- init.el
+++ init.el
@@ -93,5 +93,11 @@
 (add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
 (add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
 
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 (global-linum-mode t)
 
