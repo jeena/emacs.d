1cd3b00deaa551fa21a90a1b4f66818557765f87
Jeena <spam@jeenaparadies.net>
2016-05-09 22:35:14 +0200

    Add conf-mode to .services

--- init.el
+++ init.el
@@ -99,5 +99,7 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+(add-to-list 'auto-mode-alist '("\\.service$" . conf-mode))
+
 (global-linum-mode t)
 
